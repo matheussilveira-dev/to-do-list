7a0fbbce475ef6008afd9435c969b5d07d13b380
Matheus Silveira <silveira.matheus.dev@gmail.com>
2026-08-24 18:25:18 -0300

    feat: adicionando um validate de ID para começar o estudo de validators para as requisições

--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import tasksController from '../controllers/tasks.controller.js'
-import { validateTaskId } from '../validators/tasks.validator.js'
+import { validateId } from '../validators/tasks.validator.js'
 
 const router = express()
 
 router.get('/', tasksController.getAll)
-router.get('/:id', validateTaskId, tasksController.getById)
+router.get('/:id', validateId, tasksController.getById)
 router.post('/', tasksController.create)
-router.put('/:id', tasksController.update)
-router.delete('/:id', tasksController.deleteTask)
+router.put('/:id', validateId, tasksController.update)
+router.delete('/:id', validateId, tasksController.deleteTask)
 
 export default router
--- backend/src/validators/tasks.validator.js
+++ backend/src/validators/tasks.validator.js
@@ -1,7 +1,7 @@
-export function validateTaskId(req, res, next) {
+export function validateId(req, res, next) {
     const { id } = req.params;
 
-    if (!Number.isInteger(Number(id)) || Number(id) <= 0) {
+    if (!id || isNaN(id) || Number(id) <= 0) {
         return res.status(400).json({
             error: "ID inválido"
         });
