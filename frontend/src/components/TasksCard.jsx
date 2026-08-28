1ad0d423aebf0cddb096c43a16ae1f9f95acc093
Matheus Silveira <silveira.matheus.dev@gmail.com>
2026-08-28 18:50:19 -0300

    feat: usando dados falsos para testar os props e como vai ser quando conectar com a API

--- frontend/src/components/TasksCard.jsx
+++ frontend/src/components/TasksCard.jsx
@@ -1,13 +1,13 @@
 
 
-function TasksCard(){
+function TasksCard({title, description}){
 
     
     return(
         <article className="tasks_card">
-            <h2 className="card_title">Estudar React</h2>
+            <h2 className="card_title">{title}</h2>
 
-            <p className="card_description">Aprender components e props.</p>
+            <p className="card_description">{description}</p>
 
             <button className="card_button-edit">Editar</button>
             <button className="card_button-delete">Excluir</button>
--- frontend/src/components/TasksList.jsx
+++ frontend/src/components/TasksList.jsx
@@ -4,11 +4,35 @@ import TasksCard from "./TasksCard"
 
 function TasksList(){
 
+    const tasks = [
+    {
+        id: 1,
+        title: "Estudar React",
+        description: "Aprender componentes e props."
+    },
+    {
+        id: 2,
+        title: "Estudar Node",
+        description: "Continuar a API."
+    },
+    {
+        id: 3,
+        title: "Estudar MySQL",
+        description: "Praticar consultas."
+    }
+];
+
     return(
         <section className="tasks_list">
             <h2 className="list_title">Minhas Tarefas</h2>
 
-            <TasksCard/>
+            {tasks.map(task => (
+                <TasksCard
+                    key={task.id}
+                    title={task.title}
+                    description={task.description}
+                />
+            ))}
         </section>
     )
 }
