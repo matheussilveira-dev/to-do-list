4d82811eb9c638ec43aece08efe0c013c62112ef
Matheus Silveira <silveira.matheus.dev@gmail.com>
2026-08-28 19:28:58 -0300

    feat: exemplo de funcionamento do useState com um contador

--- frontend/src/pages/Tasks.jsx
+++ frontend/src/pages/Tasks.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react"
 
 import TasksForm from "../components/TasksForm"
 import TasksList from "../components/TasksList"
 
 function Tasks() {
+
+    const [count, setCount] = useState(0)
+
     return(
         <main>
             <h1 className="tasks_title">To-Do List</h1>
+
+            <p>Cliques: {count}</p>
+
+            <button onClick={() => setCount(count + 1)}>
+                Clique
+            </button>
 
             <TasksForm/>
 
