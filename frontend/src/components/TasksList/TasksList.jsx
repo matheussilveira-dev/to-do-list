c35d841d0ed5bf18dfcdf7ff2659fd403c200870
Matheus Silveira <silveira.matheus.dev@gmail.com>
2026-09-12 16:02:56 -0300

    style: estilo da tasklist e alterando o titulo e style

--- frontend/src/components/TasksList/TasksList.jsx
+++ frontend/src/components/TasksList/TasksList.jsx
@@ -1,11 +1,12 @@
 import TasksCard from "../TasksCard/TasksCard"
+import "./TasksList.css"
 
 function TasksList({tasks, onEditTask, onDeleteTask, loading, editingTask, onToggleTask, error}){
 
     return(
         <section className="tasks_list">
 
-            <h2 className="list_title">Minhas Tarefas</h2>
+            <h2 className="list_title">Tasks</h2>
 
             {tasks.length === 0 && !error && loading.operation !== "get"? (
                 <p>Nenhuma tarefa cadastrada.</p>
