03ff92bf9a226296e7fc2b5934aeed159f019347
Matheus Silveira <silveira.matheus.dev@gmail.com>
2026-09-14 16:20:05 -0300

    feat: alterando o summary, adicionando um title e modificando a estilização, finalizando o tasksSummary

--- frontend/src/components/TasksList/TasksList.jsx
+++ frontend/src/components/TasksList/TasksList.jsx
@@ -1,12 +1,12 @@
 import TasksCard from "../TasksCard/TasksCard"
 import "./TasksList.css"
+import TasksSummary from "../TasksSummary/TasksSummary"
 
 function TasksList({tasks, onEditTask, onDeleteTask, loading, editingTask, onToggleTask, error}){
 
     return(
         <section className="tasks_list">
-
-            <h2 className="list_title">Tasks</h2>
+          
 
             {tasks.length === 0 && !error && loading.operation !== "get"? (
                 <p>Nenhuma tarefa cadastrada.</p>
--- frontend/src/components/TasksSummary/TasksSummary.jsx
+++ frontend/src/components/TasksSummary/TasksSummary.jsx
@@ -5,20 +5,25 @@ function TasksSummary({totalTasks, pendingTasks, completedTasks}) {
 
 
     return(
-        <div className="tasks__summary-container">
-            <div className="tasks-total">
-                <p className="total-title">Total:</p>
-                <p className="total-number">{totalTasks}</p>
+        <section className="tasks__summary">
+            <div className="tasks__summary-container">
+                <h2 className="summary-title">Suas Tarefas</h2>
+                <div className="summary-infos">
+                    <div className="tasks-total">
+                        <p className="total-title">Total:</p>
+                        <p className="total-number">{totalTasks}</p>
+                    </div>
+                    <div className="tasks-pending">
+                        <p className="pending-title">Pendentes:</p>
+                        <p className="pending-number">{pendingTasks}</p>
+                    </div>
+                    <div className="tasks-completed">
+                        <p className="completed-title">Concluídas:</p>
+                        <p className="completed-number">{completedTasks}</p>
+                    </div>
+                </div>
             </div>
-            <div className="tasks-pending">
-                <p className="pending-title">Pendentes:</p>
-                <p className="pending-number">{pendingTasks}</p>
-            </div>
-            <div className="tasks-completed">
-                <p className="completed-title">Concluídas:</p>
-                <p className="completed-number">{completedTasks}</p>
-            </div>
-        </div>
+        </section> 
     )
 }
 
