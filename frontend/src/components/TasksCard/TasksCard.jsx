5e80c3e3c5623fa3e14764a4add06dd1f76f74a6
Matheus Silveira <silveira.matheus.dev@gmail.com>
2026-09-15 10:52:24 -0300

    feat: adicionando a estrutura dos cards e estilização quando estiver completada a task

--- frontend/src/components/TasksCard/TasksCard.jsx
+++ frontend/src/components/TasksCard/TasksCard.jsx
@@ -1,32 +1,44 @@
+import "./TasksCard.css"
 
+import { Check } from "lucide-react";
 
 function TasksCard({task, onEditTask, onDeleteTask, loading, editingTask, onToggleTask}){
 
     
     return(
-        <article className="tasks_card">
-            <input
-                type="checkbox"
-                checked={task.completed}
-                onChange={() => onToggleTask(task.id, !task.completed)}
-                disabled={loading.operation === "toggle" && loading.id === task.id}
-            />
+        <article className="tasks__card">
+            <div className="card__header">
+                <label className="card__checkbox">
+                    <input
+                        className="card__button-checkbox"
+                        type="checkbox"
+                        checked={task.completed}
+                        onChange={() => onToggleTask(task.id, !task.completed)}
+                        disabled={loading.operation === "toggle" && loading.id === task.id}
+                    />
+                    <span className="card__checkbox-custom">
+                        <Check 
+                        className="card__icon-checkbox"
+                        />
+                    </span>
+                </label>
+                <div className="card__informations">
+                    <h2 className={`card__title ${task.completed ? "completed" : ""}`}>{task.title}</h2>
+                    <p className="card__description">{task.description}</p>
+                </div>
+            </div>
 
-            <h2 className={`card_title ${task.completed ? "completed" : ""}`}>{task.title}</h2>
 
-            <p className="card_description">{task.description}</p>
-
-            <p className="card_id">{task.id}</p>
-
-            <button className="card_button-edit" onClick={() => onEditTask(task)}>
-                {editingTask?.id === task.id ? "Editando..." : "Editar"}
-            </button>
-
-            <button className="card_button-delete" onClick={() => onDeleteTask(task.id)} disabled={loading.operation === "delete" && loading.id === task.id}>
-                {loading.operation === "delete" && loading.id === task.id
-                    ? "Excluindo..."
-                    : "Excluir"}
-            </button>
+            <div className="card__buttons">
+                <button className="card__button-edit" onClick={() => onEditTask(task)}>
+                    {editingTask?.id === task.id ? "Editando..." : "Editar"}
+                </button>
+                <button className="card__button-delete" onClick={() => onDeleteTask(task.id)} disabled={loading.operation === "delete" && loading.id === task.id}>
+                    {loading.operation === "delete" && loading.id === task.id
+                        ? "Excluindo..."
+                        : "Excluir"}
+                </button>
+            </div>
         </article>
     )
 }
