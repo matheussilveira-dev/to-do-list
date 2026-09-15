e48358b0e598cb019610827edf17c37bae191056
Matheus Silveira <silveira.matheus.dev@gmail.com>
2026-09-15 12:06:07 -0300

    feat: adicionando estilo aos botaos e carregar durante as ações com icones estilizados

--- frontend/src/components/TasksCard/TasksCard.jsx
+++ frontend/src/components/TasksCard/TasksCard.jsx
@@ -1,6 +1,6 @@
 import "./TasksCard.css"
 
-import { Check } from "lucide-react";
+import { Check, Pencil, Trash2, LoaderCircle} from "lucide-react";
 
 function TasksCard({task, onEditTask, onDeleteTask, loading, editingTask, onToggleTask}){
 
@@ -24,19 +24,36 @@ function TasksCard({task, onEditTask, onDeleteTask, loading, editingTask, onTogg
                 </label>
                 <div className="card__informations">
                     <h2 className={`card__title ${task.completed ? "completed" : ""}`}>{task.title}</h2>
-                    <p className="card__description">{task.description}</p>
+                    <p className={`card__description ${task.completed ? "completed" : ""}`}>{task.description}</p>
                 </div>
             </div>
 
 
             <div className="card__buttons">
-                <button className="card__button-edit" onClick={() => onEditTask(task)}>
-                    {editingTask?.id === task.id ? "Editando..." : "Editar"}
+                <button
+                    className="card__button-edit"
+                    onClick={() => onEditTask(task)}
+                >
+                    {editingTask?.id === task.id ? (
+                        <LoaderCircle className="card__loading-icon"/>
+                    ) : (
+                        <Pencil className="icon-edit"/>
+                    )}
                 </button>
-                <button className="card__button-delete" onClick={() => onDeleteTask(task.id)} disabled={loading.operation === "delete" && loading.id === task.id}>
-                    {loading.operation === "delete" && loading.id === task.id
-                        ? "Excluindo..."
-                        : "Excluir"}
+                <button
+                    className="card__button-delete"
+                    onClick={() => onDeleteTask(task.id)}
+                    disabled={
+                        loading.operation === "delete" &&
+                        loading.id === task.id
+                    }
+                >
+                    {loading.operation === "delete" &&
+                    loading.id === task.id ? (
+                        <LoaderCircle className="card__loading-icon"/>
+                    ) : (
+                        <Trash2 className="icon-delete"/>
+                    )}
                 </button>
             </div>
         </article>
